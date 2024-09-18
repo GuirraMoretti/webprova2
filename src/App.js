e17f5ace10b719037b27293074cebd4101509716
Guirra <guilherme.moretti@alu.ufc.br>
2024-09-18 08:33:52 -0300

    [init] - Iniciando repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainPage from './components/13MUI/Crud/MainPage';
+
+function App() {
+  return (
+    <div>        
+        <MainPage />
+    </div>
+  );
+}
+
+export default App;
